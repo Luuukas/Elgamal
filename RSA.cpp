c65d1690e56e1bcb4857e5c87beae04f4e092af7
Luuukas <17ycchen4@stu.edu.cn>
2020-02-29 17:02:24 +0800

    multithreads

--- RSA.cpp
+++ RSA.cpp
@@ -3,11 +3,12 @@
 #include <memory.h>
 #include <assert.h>
 #include <random>
+#include <time.h>  
 using namespace std;
 typedef long long ll;
 // FFT
 const double pi = acos(-1);
-const int mxi = 5;
+const int mxi = 16;
 constexpr int to2exp(int x){
     int cnt1 = 0;
     int cnti = 0;
@@ -69,8 +70,9 @@ void FFT(int I, complex<double > *a, int n){    // 傅里叶变换 or 插值
         a[i] /= n;
     }
 }
-complex<double > t[N<<1];
 class bigNum{
+private:
+    complex<double > t[N<<1];
 public:
     // static complex<double > t[N<<1];    // undefined reference to `bigNum::t'
 
@@ -78,6 +80,16 @@ public:
 
     bigNum(const complex<double > *b){
         memcpy(a,b,sizeof(a));
+    }
+
+    bigNum(const char num[]){
+        if(num==NULL) bigNum(0);
+        else{
+            int l = strlen(num);
+            for(int i=0;i<l;++i){
+                a[i] = num[l-1-i]-'0';
+            }
+        }
     }
 
     bigNum(int c){
@@ -259,6 +271,72 @@ bigNum pow_mod(bigNum a, bigNum n, bigNum &mod){
     }
     return res;
 }
+bigNum gcd(bigNum a, bigNum b){
+    if(b.is0()) return a;
+    bigNum c = DIV(a, b);
+    bigNum d = b;
+    d *= c;
+    a -= d;
+    return gcd(b, a);
+}
+bigNum ext_gcd(bigNum a, bigNum b, int &ss, bigNum& s, int &st, bigNum& t){
+    if(b.is0()){
+        ss = 1;
+        s = bigNum(0);
+        s.add1();
+        st = 1;
+        t = bigNum(0);
+        return a;
+    }
+    bigNum c = DIV(a, b);    // c=a/b
+    bigNum d = b;
+    d *= c;
+    a -= d;    // a=a%b
+    bigNum g = ext_gcd(b,a,ss,s,st,t);
+    cout << "ext_gcd: ";
+    cout << (ss==-1?"-":"");s.print();cout << " * ";b.print();cout << " + " << (st==-1?"-":"");t.print();cout << " * ";a.print();cout << " = ";g.print();cout << endl;
+    int tss = ss;
+    bigNum ts = s;
+    ss = st;
+    s = t;
+    t *= c;
+    if(tss==1&&st==1){
+        if(t>ts){
+            st = -1;
+            t -= ts;
+        }else{
+            st = 1;
+            ts -= t;
+            t = ts;
+        }
+    }else if(tss==1&&st==-1){
+        st = 1;
+        t += ts;
+    }else if(tss==-1&&st==1){
+        st = -1;
+        t += ts;
+    }else if(tss==-1&&st==-1){
+        if(ts>t){
+            st = -1;
+            ts -= t;
+            t = ts;
+        }else{
+            st = 1;
+            t -= ts;
+        }
+    }
+    return g;
+}
+bigNum inv_mod(bigNum a, bigNum b){
+    bigNum s(0), t(0);
+    int ss, st;
+    bigNum g = ext_gcd(a,b,ss,s,st,t);
+    cout << "g: ";g.print();cout << endl;
+    t *= b;
+    t.add1();
+    t = DIV(t, a);
+    return t;
+}
 bigNum generate(int N, bigNum *limit){
     assert(N<=mxi);
     bigNum p(0);
@@ -274,11 +352,11 @@ bigNum generate(int N, bigNum *limit){
 // 留坑：卡迈克尔(Carmichael)数
 bool Miller_Rabin(bigNum &n, int t, int N){
     if(n.is0()||n.is1()) return false;
-    if(n.isplural()){
-        n.div2();
-        return n.is1();
-    }
     bigNum d = n;
+    if(n.isplural()){
+        d.div2();
+        return d.is1();
+    }
     d.sub1();
     int s = 0;
     while(!d.is0()&&d.isplural()){
@@ -301,34 +379,136 @@ bool Miller_Rabin(bigNum &n, int t, int N){
     }
     return true;
 }
+
 bigNum generate_prime(int N, int k, int t){
+    // clock_t start, finish;    
+    // double  duration;
+    // start = clock();
     for(int i=0;i<k;++i){
         bigNum n = generate(N,nullptr);
-        if(Miller_Rabin(n,t,N)) return n;
+        if(Miller_Rabin(n,t,N)){
+            // finish = clock();
+            // duration = (double)(finish - start) / CLOCKS_PER_SEC;
+            // cout << "used " << duration << " secs to generate a prime." << endl;
+            return n;
+        }
     }
     return bigNum(0);
 }
-bigNum PrimitiveRoot(int N, bigNum &p){
-    bigNum bN2(0);
-    bN2.a[0] = 2;
-    bigNum q(p);
-    q *= bN2;
-    q.add1();
-    bigNum g(0);
-    bigNum pg2(0), pgq(0);
-    do{
-        g = generate(N, &p);
-        pg2 = pow_mod(g,bN2,p);
-        pgq = pow_mod(g,q,p);
-    }while(pg2.is1()||pgq.is1());
-    return g;
+#include <pthread.h>
+// 开启threadnum个线程，每个线程都在循环求可行的bigNum，直到超过总的尝试次数，或有一个可行bigNum产生则退出线程
+pthread_mutex_t cnt_mutex;
+int calcnt;
+int totcnt;
+pthread_mutex_t res_mutex;
+bool hasres;
+bigNum res(0);
+const int mxth = 5;    // 最大的支持线程数
+pthread_t threads[mxth];    // 保存所有线程的句柄
+void *generate_prime_thread(void *arg){
+    int *t = (int*)arg;
+    while(!hasres&&calcnt<totcnt){
+        pthread_mutex_lock(&cnt_mutex);
+        ++calcnt;
+        pthread_mutex_unlock(&cnt_mutex);
+        bigNum n = generate(t[0],nullptr);
+        cout << "working in thread " << t[2] << ", trying ";n.print();cout << endl;
+        if(Miller_Rabin(n,t[1],t[0])){
+            pthread_mutex_lock(&res_mutex);
+            hasres = true;
+            res = n;
+            pthread_mutex_unlock(&res_mutex);
+        }
+    }
 }
+void pthread_join_all(pthread_t threads[], int threadnum){
+    for(int i=0;i<threadnum;i++){
+        pthread_join(threads[i],NULL);
+    }
+}
+bigNum generate_prime_multithreaded(int N, int k, int t, int threadnum){
+    // clock_t start, finish;    
+    // double  duration;
+    // start = clock();
+    calcnt = 0;
+    totcnt = k;
+    hasres = false;
+    pthread_mutex_init(&cnt_mutex, NULL);
+    pthread_mutex_init(&res_mutex, NULL);
+    for(int i=0;i<threadnum;i++)
+    {
+        int arg[3] = {N,t,i};
+        pthread_create(&threads[i], NULL, generate_prime_thread, (void*)&arg);
+    }
+    pthread_join_all(threads, threadnum);
+    pthread_mutex_destroy(&cnt_mutex);
+    pthread_mutex_destroy(&res_mutex);
+    // finish = clock();
+    // duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    // cout << "used " << duration << " secs to generate a prime." << endl;
+    return res;
+}
+
+const int mxk = 100;
+const int mxt = 100;
 int main(){
-    bigNum res = generate_prime(mxi,100,100);
-    res.print();
-    cout << endl;
-    bigNum resg = PrimitiveRoot(mxi,res);
-    resg.print();
-    cout << endl;
+    // // 该部分用于对比单线程与多线程的生成效率
+    // bigNum prime = generate_prime(10,100,100);
+    // // bigNum prime = generate_prime_multithreaded(10,100,100,5);
+    // cout << "prime: ";prime.print();cout << endl;
+
+    int pi, qi;
+    cout << "Input the digits of prime numbers p and q respectively (up to "<< mxi/2 << "): ";
+    cin >> pi >> qi;
+    // 小BUG：如果最终生成的素数p<q，则不能正确加密解密。所以最好输入的pi>qi来保证p>q。
+    bigNum p = generate_prime(pi,mxk,mxt), q = generate_prime(qi,mxk,mxt);
+    if(p.is0()||q.is0()){
+        cout << "Cannot find two prime numbers." << endl;
+        exit(1);
+    }
+    cout << "p: ";p.print();cout << endl;
+    cout << "q: ";q.print();cout << endl;
+    bigNum phip = p, phiq = q;
+    bigNum n = p;
+    n *= q;
+    p.sub1();
+    q.sub1();
+    p *= q;
+    bigNum &phin = p;
+    bigNum e(0);
+    int ei = pi + qi;
+    cout << "n: ";n.print();cout << endl;
+    cout << "phin: ";phin.print();cout << endl;
+    do{
+        e = generate(ei,NULL);
+        cout<< "try e: ";e.print();cout << endl;
+    }while(!gcd(e,phin).is1());
+    MOD(e, phin);
+    bigNum d = inv_mod(e,phin);
+    cout << "Public key: (n: ";n.print();cout << ", e: ";e.print();cout << ")" << endl;
+    cout << "Private key: (n: ";n.print();cout << ", d: ";d.print();cout << ")" << endl;
+
+    cout << "e.g. " << endl;
+    char numM[] = "6451";
+    bigNum M(numM);
+    cout << "M: ";M.print();cout << endl;
+    M = pow_mod(M, e, n);
+    cout << "C(encrypted M): ";M.print();cout << endl;
+    M = pow_mod(M, d, n);
+    cout << "M(decrypted C): ";M.print();cout << endl;
+
+    // // 该部分用于验证大数gcd的正确性
+    // bigNum rnd1 = generate(5,NULL);
+    // rnd1.print();
+    // cout << endl;
+    // bigNum rnd2 = generate(5,NULL);
+    // rnd2.print();
+    // cout << endl;
+    // bigNum res = gcd(rnd1, rnd2);
+    // res.print();
+    // cout << endl;
+    // bigNum res = generate_prime(mxi,100,100);
+    // res.print();
+    // cout << endl;
     return 0;
 }
